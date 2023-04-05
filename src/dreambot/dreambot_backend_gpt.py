ec7ed75be00da7563dfd63251ec49b5ed271430c
Chris Jones <cmsj@tenshu.net>
2023-04-05 13:24:03 +0100

    Allow GPT backend to maintain conversation state if users desire

--- src/dreambot/dreambot_backend_gpt.py
+++ src/dreambot/dreambot_backend_gpt.py
@@ -16,6 +16,7 @@ class DreambotBackendGPT(dreambot_backend_base.DreambotBackendBase):
     organization = None
     model = None
     backend_name = "GPT"
+    chat_cache = {}
 
     def __init__(self, nats_options, gpt_options):
         super().__init__(nats_options)
@@ -24,17 +25,41 @@ class DreambotBackendGPT(dreambot_backend_base.DreambotBackendBase):
         self.model = gpt_options["model"]
         logger.debug("Set GPT options to: api_key={}, organization={}, model={}".format(self.api_key, self.organization, self.model))
 
+    def cache_name_for_prompt(self, data):
+        return "{}_{}_{}".format(data["reply-to"], data["channel"], data["user"])
+
+    def reset_cache(self, key):
+        self.chat_cache[key] = [{"role": "system", "content": "You are a helpful assistant. Make your answers as brief as possible."}]
+
     async def boot(self):
         def gpt_callback(data):
             try:
-                response = openai.ChatCompletion.create(
-                    model = self.model,
-                    messages = [
-                        {"role": "user", "content": "Limit your responses to 500 characters. {}".format(data["prompt"])},
-                    ]
-                )
+                prompt = data["prompt"]
+                message = {"role": "user", "content": prompt}
+
+                # Ensure we have a valid cache line for this user
+                cache_key = self.cache_name_for_prompt(data)
+                if cache_key not in self.chat_cache:
+                    logger.debug("Creating new cache entry for {}".format(cache_key))
+                    self.reset_cache(cache_key)
+
+                # Determine if we're adding to the cache or starting a new conversation
+                if not prompt.startswith("!followup"):
+                    logger.debug("Starting new conversation for '{}'".format(cache_key))
+                    self.reset_cache(cache_key)
+                else:
+                    logger.debug("Adding to existing conversation for '{}'".format(cache_key))
+
+                # Now that our cache is in the right state, add this new prompt to it
+                self.chat_cache[cache_key].append(message)
+
+                # Now we can ask OpenAI for a response to the contents of our message cache
+                response = openai.ChatCompletion.create(model = self.model, messages = self.chat_cache[cache_key])
+
+                # Fetch the response, prepare it to be sent back to the user and added to their cache
                 reply = response.choices[0].message.content
                 data["reply-text"] = reply
+                self.chat_cache[cache_key].append({"role": "assistant", "content": reply})
             except (APIError, Timeout, ServiceUnavailableError) as e:
                 logger.error("GPT service access error: {}".format(e))
                 data["error"] = "GPT service unavailable, try again."
