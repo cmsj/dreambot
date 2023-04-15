275535394700c5f47f036687da6e234312d7b9de
Chris Jones <cmsj@tenshu.net>
2023-04-15 22:34:10 +0100

    Start adding tests for cli.py

---
+++ tests/test_cli.py
@@ -0,0 +1,38 @@
+import pytest
+import dreambot.shared.cli
+from unittest.mock import patch, AsyncMock, MagicMock
+
+
+def test_cli_parseargs():
+    cli = dreambot.shared.cli.DreambotCLI()
+    with patch("sys.argv", ["main", "-c", "somefile.json"]):
+        cli.parse_args()
+    assert cli.args is not None
+
+
+def test_boot_missing_nats_uri():
+    m = MagicMock(side_effect=[{"foo": "bar"}])
+    p1 = patch("builtins.open", MagicMock())
+    p2 = patch("json.load", m)
+
+    cli = dreambot.shared.cli.DreambotCLI()
+    with patch("sys.argv", ["main", "-c", "somefile.json"]):
+        with p1 as p_open:
+            with p2 as p_json_load:
+                with pytest.raises(ValueError):
+                    cli.boot()
+
+
+def test_boot():
+    m = MagicMock(side_effect=[{"nats_uri": "bar"}])
+    p1 = patch("builtins.open", MagicMock())
+    p2 = patch("json.load", m)
+    patch("dreambot.shared.nats.NatsManager", MagicMock())
+
+    cli = dreambot.shared.cli.DreambotCLI()
+    with patch("sys.argv", ["main", "-c", "somefile.json"]):
+        with p1 as p_open:
+            with p2 as p_json_load:
+                cli.boot()
+    assert cli.options == {"nats_uri": "bar"}
+    assert cli.nats is not None
