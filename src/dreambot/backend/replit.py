7e17642e4e4f311cf0d924bf8fa4aa9ebd165b55
Chris Jones <cmsj@tenshu.net>
2023-05-04 11:31:16 +0100

    Force replit onto the GPU

--- src/dreambot/backend/replit.py
+++ src/dreambot/backend/replit.py
@@ -2,7 +2,8 @@ import json
 
 from typing import Any, Callable, Coroutine
 from argparse import REMAINDER, ArgumentError
-from transformers import AutoModelForCausalLM, AutoTokenizer  # type: ignore
+from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed  # type: ignore
+import torch
 
 from dreambot.backend.base import DreambotBackendBase
 from dreambot.shared.worker import UsageException, ErrorCatchingArgumentParser
@@ -13,11 +14,18 @@ class DreambotBackendReplit(DreambotBackendBase):
         self, options: dict[str, Any], callback_send_workload: Callable[[str, bytes], Coroutine[Any, Any, None]]
     ):
         super().__init__("Replit", options, callback_send_workload)
+        self.hf_token = options["hugging_face_token"]
 
     async def boot(self):
         self.logger.info("Booting model...")
-        self.tokenizer = AutoTokenizer.from_pretrained("replit/replit-code-v1-3b", trust_remote_code=True)  # type: ignore
-        self.model = AutoModelForCausalLM.from_pretrained("replit/replit-code-v1-3b", trust_remote_code=True)  # type: ignore
+        self.tokenizer = AutoTokenizer.from_pretrained("replit/replit-code-v1-3b", use_auth_token=self.hf_token, trust_remote_code=True)  # type: ignore
+        self.tokenizer.truncation_side = "left"  # type: ignore
+
+        self.model = AutoModelForCausalLM.from_pretrained("replit/replit-code-v1-3b", use_auth_token=self.hf_token, trust_remote_code=True)  # type: ignore
+        self.model.to(device="cuda", dtype=torch.bfloat16)  # type: ignore
+
+        self.model.eval()  # type: ignore
+        self.logger.info("Booted")
 
     async def shutdown(self):
         return
@@ -37,16 +45,21 @@ class DreambotBackendReplit(DreambotBackendBase):
 
             # Tokenize our prompt and do inference
             self.logger.debug("Sending request to Replit...")
-            tokens = self.tokenizer.encode(args.prompt, return_tensors="pt")  # type: ignore
+            tokens = self.tokenizer.encode(args.prompt, return_tensors="pt", max_length=1024, truncation=True)  # type: ignore
+            tokens.to("cuda")  # type: ignore
+
+            set_seed(args.seed)
+
             generation = self.model.generate(  # type: ignore
                 tokens,
-                max_length=100,
-                do_sample=True,
-                top_p=0.95,
-                top_k=4,
+                max_new_tokens=48,  # FIXME: Make this an arg. Min 8, Max 128, steps 1, default 48
                 temperature=args.temperature,
-                num_return_sequences=1,
+                pad_token_id=self.tokenizer.pad_token_id,  # type: ignore
                 eos_token_id=self.tokenizer.eos_token_id,  # type: ignore
+                top_p=0.95,  # FIXME: Make this an arg, see https://huggingface.co/spaces/replit/replit-code-v1-3b-demo/blob/main/app.py
+                top_k=4,  # FIXME: Ditto
+                use_cache=True,  # FIXME: Ditto
+                repetition_penalty=1.0,  # FIXME: Ditto
             )
 
             # Decoding our result
@@ -78,6 +91,7 @@ class DreambotBackendReplit(DreambotBackendBase):
 
     def arg_parser(self) -> ErrorCatchingArgumentParser:
         parser = super().arg_parser()
+        parser.add_argument("-s", "--seed", help="Random seed for the model", default=42, type=int)
         parser.add_argument(
             "-t",
             "--temperature",
